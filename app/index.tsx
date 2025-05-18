c3030fec96a9f249c8ff97f94a9af0ec4d69f6f0
baont <thaibaoo2105@gmail.com>
2025-05-18 16:56:05 +0700

    set up project
    a

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-5xl text-white">Hello Tailwind</Text>
+    </View>
+  );
+}
